84d1327d56791bf959e8230a2215e698f4fe093c
enamulhaq <enamulbokshi@gmail.com>
2025-02-27 02:01:28 +0600

    shadcn ui library added to project

--- .next/static/chunks/app_layout_tsx_61af54._.js
+++ .next/static/chunks/app_layout_tsx_61af54._.js
@@ -6,7 +6,9 @@
   "path": "static/chunks/app_layout_tsx_61af54._.js",
   "chunks": [
     "static/chunks/[root of the server]__9d1267._.css",
-    "static/chunks/app_layout_tsx_83fc40._.js"
+    "static/chunks/node_modules_f8f399._.js",
+    "static/chunks/_0b4fef._.js",
+    "static/chunks/node_modules_@clerk_nextjs_dist_esm_app-router_a9daba._.js"
   ],
   "source": "dynamic"
 });
--- components/navs/PrimaryNav.tsx
+++ components/navs/PrimaryNav.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
-
+"use client"
+import Image from "next/image"
+import {SignedIn,SignedOut,useUser} from "@clerk/nextjs"
+import { Button } from "../ui/button"
 function PrimaryNav() {
+  const {isLoaded, isSignedIn,user } = useUser()
+  if(!isLoaded) return <p>Loading...</p>
+
   return (
     <nav className='flex justify-between items-center py-4 px-5 bg-white shadow-md'>
         <div className=''>
@@ -10,6 +15,21 @@ function PrimaryNav() {
             <a href='#' className='text-gray-600 hover:text-gray-800'>Home</a>
             <a href='#' className='text-gray-600 hover:text-gray-800'>About</a>
             <a href='#' className='text-gray-600 hover:text-gray-800'>Contact</a>
+        </div>
+        <div>
+            <SignedIn >
+                <div className="flex gap-3">
+                {
+                  user?.imageUrl ? <Image src={user.imageUrl} alt={user?.fullName || "profile avtar"} width={40} height={40} className='rounded-full' /> : <a href='#' className='text-gray-600 hover:text-gray-800'>Profile</a>
+                  
+                }
+                <Button variant='destructive' >Sign Out</Button>
+                </div>
+            </SignedIn>
+            <SignedOut>
+                <a href='/sign-in/' className='text-gray-600 hover:text-gray-800'>Sign In</a>
+                <a href=""></a>
+            </SignedOut>
         </div>
     </nav>
   )
